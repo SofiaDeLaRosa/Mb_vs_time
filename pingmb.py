96039adf8073232865986da8c9fe764502e436fb
SofiaDeLaRosa <100147009+SofiaDeLaRosa@users.noreply.github.com>
2022-06-14 23:06:19 -0500

    Update pingmb.py

--- pingmb.py
+++ pingmb.py
@@ -1,9 +1,8 @@
 import subprocess,sys,re
 
-MB="A"*(1024*1024)
+MB="."*(1024*1024)
 
 def pingmb(ip,MBs,verbose=False):
-    #subprocess.run("echo \"LIIINUUUUX\" > tmp", shell=True)
     with open('tmp','w') as t:
         for i in range(0,int(MBs)):
             t.write(MB)
